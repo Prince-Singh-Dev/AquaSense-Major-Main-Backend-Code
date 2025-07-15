1c8d8a25313a42beb57d5b051956fe54daddd73e
Prince Singh <princesingh6173@gmail.com>
2025-07-15 14:40:47 +0530

    Update memoryStore.js

--- memoryStore.js
+++ memoryStore.js
@@ -1,14 +1,21 @@
 let command = "OFF";
 let status = "unknown";
+let statusSource = "unknown";
 let level = 0;
 
 module.exports = {
+  // App Command (desired state)
   getCommand: () => command,
   setCommand: (c) => { command = c; },
 
-  getStatus: () => status,
-  setStatus: (s) => { status = s; },
+  // Actual Motor Status (from ESP)
+  getStatus: () => ({ status, source: statusSource }),
+  setStatus: (s, source = "ESP") => {
+    status = s;
+    statusSource = source;
+  },
 
+  // Water Level
   getLevel: () => level,
   setLevel: (l) => { level = l; }
 };
